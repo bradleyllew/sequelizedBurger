527dfa49ba336551e4622ac8e80d9facc80e9d14
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-10-28 17:59:40 -0400

    making sure everything is updated

--- seeders/seeds.sql
+++ seeders/seeds.sql
@@ -1,7 +1,4 @@
-ALTER TABLE Burgers MODIFY createdAt VARCHAR(255) DEFAULT NULL;
-ALTER TABLE Burgers MODIFY updatedAt VARCHAR(255) DEFAULT NULL;
-ALTER TABLE Customer MODIFY createdAt VARCHAR(255) DEFAULT NULL;
-ALTER TABLE Customer MODIFY updatedAt VARCHAR(255) DEFAULT NULL;
+
 INSERT INTO burgers (burger_name) VALUES ('Bradley Bean Burger');
 INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
 INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
